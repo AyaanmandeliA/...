af1922f21216f6f027683786c8f8c97bf992dba7
AyaanmandeliA <78735976+AyaanmandeliA@users.noreply.github.com>
2022-02-22 15:59:55 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
-function setup(){
-    canvas=createCanvas(396 , 396);
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(396, 396)
+    canvas = createCanvas(396, 396);
+    canvas.position(410, 151);
+
+    posenet = ml5.poseNet(video, modelLoaded);
+    posenet.on("pose", getposes);
+}
+function draw() {
+    background("yellow");
+}
+function modelLoaded() {
+    console.log("model loaded");
+}
+function getposes(results) {
+    if (results.length > 0) {
+        console.log(results);
+    }
 }
