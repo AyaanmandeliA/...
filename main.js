29abaf9c8a0bad73fc0202c2b269641f8ee099cf
AyaanmandeliA <78735976+AyaanmandeliA@users.noreply.github.com>
2022-03-03 15:27:25 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+nose_x = 0;
+nose_y = 0;
+
+wl = 0;
+wr = 0;
+
+lolidc = 0;
+
 function setup() {
     video = createCapture(VIDEO);
     video.size(396, 396)
@@ -9,6 +17,11 @@ function setup() {
 }
 function draw() {
     background("yellow");
+    background("yellow");
+    fill("red");
+    stroke("red");
+    square(nose_x , nose_y , lolidc);
+    document.getElementById("square_sides").innerHTML = "Side of the square will be "+lolidc+" px";
 }
 function modelLoaded() {
     console.log("model loaded");
@@ -16,5 +29,15 @@ function modelLoaded() {
 function getposes(results) {
     if (results.length > 0) {
         console.log(results);
+        nose_x = results[0].pose.nose.x;
+        nose_y = results[0].pose.nose.y;
+        console.log("nose x = "+nose_x +" nose y = "+nose_y);
+
+        wl = results[0].pose.leftWrist.x;
+        wr = results[0].pose.rightWrist.x;
+        console.log("left = "+wl +" right = "+wr);
+
+        lolidc = wl - wr;
+        console.log(lolidc);
     }
 }
